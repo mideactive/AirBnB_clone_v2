606caf613b2d1c83f6a7e31f0732c1d32ac72aac
Ubuntu <jenyoolumide@yahoo.com>
2023-07-05 21:15:38 +0000

    preparing web server

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,31 +1,12 @@
 #!/usr/bin/env bash
-
-# Install Nginx if not already installed
-if ! dpkg-query -W -f='${Status}' nginx | grep "installed" >/dev/null; then
-    apt-get -y update
-    apt-get -y install nginx
+#setup inital server conf
+if [ ! -x /usr/sbin/nginx ]; then
+        apt-get update
+        apt-get install nginx -y
 fi
-
-# Create necessary directories
-mkdir -p /data/web_static/releases/test/
-mkdir -p /data/web_static/shared/
-
-# Create a fake HTML file
-echo -e "<html>\n  <head>\n  </head>\n  <body>\n    Holberton School\n  </body>\n</html>" > /data/web_static/releases/test/index.html
-
-# Create symbolic link
-if [ -L /data/web_static/current ]; then
-    rm /data/web_static/current
-fi
-ln -s /data/web_static/releases/test/ /data/web_static/current
-
-# Give ownership to the ubuntu user and group
-chown -R ubuntu:ubuntu /data/
-
-# Update Nginx configuration
-config_path="/etc/nginx/sites-available/default"
-sed -i '/^\tlocation \/ {/a\\n\t\tlocation /hbnb_static/ {\n\t\t\talias /data/web_static/current/;\n\t\t}' $config_path
-
-# Restart Nginx
+mkdir -p /data/web_static/shared/ /data/web_static/releases/test/
+echo "fake content" | sudo tee /data/web_static/releases/test/index.html
+ln -nsf /data/web_static/releases/test/ /data/web_static/current
+chown ubuntu:ubuntu -R /data/
+sed -i '72i\\tlocation /hbnb_static/ {\n\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-enabled/default
 service nginx restart
-
