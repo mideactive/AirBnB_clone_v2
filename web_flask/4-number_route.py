9b9f5ea984042add40bc28f118601cfa246d2a70
mideactive <jenyoolumide@yahoo.com>
2023-07-22 16:26:42 +0000

    start flask and display nummeric variable

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -10,16 +10,19 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hbnb_web():
+    """display a string"""
     return "Hello HBNB!"
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb_web1():
+    """display a string"""
     return "HBNB"
 
 
 @app.route('/c/<text>', strict_slashes=False)
 def c_route(text):
+    """display a variable"""
     return 'C {}'.format(escape(text.replace('_', ' ')))
 
 
@@ -27,11 +30,13 @@ def c_route(text):
 @app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def p_route(text):
+    """display a variable"""
     return 'Python {}'.format(escape(text.replace('_', ' ')))
 
 
 @app.route('/number/<int:n>', strict_slashes=False)
 def n_route(n):
+    """display an int vairable"""
     return '{} is a number'.format(n)
 
 
